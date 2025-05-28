cceb43eaee59b0737d9b1eb2f6af5e745f60a293
GucciBoyPasta <nicholasgrailed@gmail.com>
2025-05-27 20:34:14 -0400

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'Comp Time Calculator',
+  description: 'DS-5106 style travel comp time tracker',
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
