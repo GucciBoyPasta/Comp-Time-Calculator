3341e17a9d6ef8afe33063c62fbaf1664c942bcf
GucciBoyPasta <nicholasgrailed@gmail.com>
2025-05-27 20:36:47 -0400

    Delete ui directory

--- ui/input.js
+++
@@ -1,3 +0,0 @@
-export function Input({ type, value, onChange, placeholder }) {
-  return <input type={type} value={value} onChange={onChange} placeholder={placeholder} className="border p-2 rounded w-full" />;
-}
